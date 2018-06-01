690c1f7dc3d21f9764bd254f06c4d6f6bcd99efc
Charlie.Welch <cmw24400@ucmo.edu>
2018-06-01 00:11:30 -0500

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,8 @@ $(document).ready(function() {
     var words = ["young-the-giant", "phoenix", "twenty-one-pilots", "glass-animals", "rainbow-kitten-surprise", "mumford-and-sons"];
     var currentWord = words[Math.floor(Math.random()* words.length)];
     var answerTokens = [];
+    var indexesFound = [];
+    var lastIndex
     for (i=0; i < currentWord.length; i++){
         var space = $("<h5>");
         $("#current-word").append(space);
@@ -19,15 +21,23 @@ $(document).ready(function() {
     $("#current-word").text(answerTokens.join(" "));
 
     $(document).keyup(function(event) {
-        var indexesFound = [];
+        // var indexesFound = [];
+        // var lastIndex = 0;
         var userGuess = event.key.toLowerCase();
         console.log(userGuess);
-        var index = currentWord.indexOf(userGuess);
-        console.log(index);
-        if (index != -1) {
-            indexesFound.push(index);
-            console.log(indexesFound);
+
+        while (lastIndex != -1) {
+            lastIndex = currentWord.indexOf(userGuess, lastIndex);
+            console.log(lastIndex);
+
+            if(lastIndex != -1){
+                indexesFound.push(lastIndex);
+                console.log(indexesFound.join(" "));
+            }
+            return indexesFound;
         }
+        
+
     })
     
 
