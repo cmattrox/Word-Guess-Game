3711187617281aa15081f959cf9ff4d62ebf6c73
Charlie.Welch <cmw24400@ucmo.edu>
2018-05-31 21:18:23 -0500

    updated commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,23 +2,33 @@ $(document).ready(function() {
     var wins = 0;
     var guesses = 8;
     var words = ["young-the-giant", "phoenix", "twenty-one-pilots", "glass-animals", "rainbow-kitten-surprise", "mumford-and-sons"];
-    var currentword = words[Math.floor(Math.random()* words.length)];
+    var currentWord = words[Math.floor(Math.random()* words.length)];
     var answerTokens = [];
-    for (i=0; i < currentword.length; i++){
-        console.log(currentword.length);
-        console.log(currentword)
+    for (i=0; i < currentWord.length; i++){
         var space = $("<h5>");
         $("#current-word").append(space);
         
-        if(currentword[i] === "-" || currentword[i] === " "){
-            answerTokens.push(currentword[i]);
+        if(currentWord[i] === "-" || currentWord[i] === " "){
+            answerTokens.push(currentWord[i]);
         }
         else {
-            answerTokens.push("_")
+            answerTokens.push("_");
         }
     }
     
-    $("<h5>").text(answerTokens.join(" "));
+    $("#current-word").text(answerTokens.join(" "));
+
+    $(document).keyup(function(event) {
+        var indexesFound = [];
+        var userGuess = event.key.toLowerCase();
+        console.log(userGuess);
+        var index = currentWord.indexOf(userGuess);
+        console.log(index);
+        if (index != -1) {
+            indexesFound.push(index);
+            console.log(indexesFound);
+        }
+    })
     
 
 
